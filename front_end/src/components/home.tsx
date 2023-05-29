0c828b66e4296acbaf5204036915ee5d105c6e3a
Qoolthard <kulcsarlaszloattila@gmail.com>
2023-05-29 19:50:40 +0200

    Creat Main Page

--- front_end/src/components/home.tsx
+++ front_end/src/components/home.tsx
@@ -1,7 +1,37 @@
 import * as React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const containerStyle: React.CSSProperties = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    textAlign: "center",
+  };
+
+  const textStyle: React.CSSProperties = {
+    fontSize: "24px",
+    marginBottom: "16px",
+    marginTop: "32px",
+    marginLeft: "150px",
+  };
+
+  const imageStyle = {
+    border: "2px",
+    width: "800px",
+    height: "auto", 
+    marginLeft: "150px",
+  };
+
+  return (
+    <div style={containerStyle}>
+      <div style={textStyle}><b>Köszöntjük Webáruházunkban!</b></div>
+      <div>
+        <img src="/home.jpg" style={imageStyle} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
